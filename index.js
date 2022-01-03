9c2e1b7746f5e0c4815c1c260f3882218b5d1317
Saad100912 <arsaad.2016@gmail.com>
2022-01-03 20:04:24 +0600

    payment

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const cors = require("cors");
 const { MongoClient } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const fileUpload = require("express-fileupload");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.801m1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -33,6 +36,13 @@ async function run() {
             const products = await cursor.toArray();
             res.json(products);
         });
+        // Get all Orders
+
+        app.get("/orders", async (req, res) => {
+            const cursor = ordersCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
 
         // Get limited products for home page
         app.get("/home/products", async (req, res) => {
@@ -47,6 +57,13 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const product = await productsCollection.findOne(query);
             res.json(product);
+        });
+        // Get order by id
+        app.get("/orders/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.findOne(query);
+            res.json(result);
         });
 
         // Check if the user is admin or not
@@ -68,14 +85,6 @@ async function run() {
             const orders = ordersCollection.find(query);
             const result = await orders.toArray();
             res.json(result);
-        });
-
-        // Get order by id
-        app.get("/orders/:id", async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const order = ordersCollection.find(query);
-            res.json(order);
         });
 
         // Get orders
@@ -165,6 +174,17 @@ async function run() {
             const result = await productsCollection.deleteOne(query);
             res.json(result);
         });
+
+        app.post("/create-payment-intent", async (req, res) => {
+            const paymentInfo = req.body;
+            const amount = paymentInfo.price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: "usd",
+                amount: amount,
+                payment_method_types: ["card"],
+            });
+            res.json({ clientSecret: paymentIntent.client_secret });
+        });
     } finally {
         // await client.close();
     }
@@ -172,9 +192,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("Kids Island server is running");
+    res.send("Baby Land server is running");
 });
 
 app.listen(port, () => {
-    console.log("Kids Island server is running on:", port);
+    console.log("Baby Land server is running on:", port);
 });
