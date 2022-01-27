2357f38cdd6ac247b2d6c108a8bf2ccb99640377
Saad100912 <arsaad.2016@gmail.com>
2022-01-27 20:56:23 +0600

    commit

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const cors = require("cors");
 const { MongoClient } = require("mongodb");
 const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const fileUpload = require("express-fileupload");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.801m1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -20,53 +23,41 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         await client.connect();
-        const database = client.db("travellers_hut");
-        const blogsCollection = database.collection("blogs");
+        const database = client.db("kids_island");
+        const productsCollection = database.collection("products");
         const usersCollection = database.collection("users");
+        const ordersCollection = database.collection("orders");
+        const reviewCollection = database.collection("reviews");
 
         /* GET */
-
-        // Get all the blogs
-        app.get("/manageBlogs", async (req, res) => {
-            const cursor = blogsCollection.find({});
-            const blogs = await cursor.toArray();
-            res.json(blogs);
+        // Get all the products
+        app.get("/products", async (req, res) => {
+            const cursor = productsCollection.find({});
+            const products = await cursor.toArray();
+            res.json(products);
         });
 
-        // Get all the approved blogs with pagination function
-        app.get("/blogs", async (req, res) => {
-            const query = { status: "approved" };
-            const cursor = blogsCollection.find(query);
-            const page = req.query.page;
-            const size = parseInt(req.query.size);
-            let blogs;
-            const count = await cursor.count();
-            if (page) {
-                blogs = await cursor
-                    .skip(page * size)
-                    .limit(size)
-                    .toArray();
-            } else {
-                blogs = await cursor.toArray();
-            }
-            res.send({ count, blogs });
+        // Get limited products for home page
+        app.get("/home/products", async (req, res) => {
+            const cursor = productsCollection.find({});
+            const products = await cursor.limit(6).toArray();
+            res.json(products);
         });
 
-        // Get the pending blogs
-        app.get("/pendingBlogs", async (req, res) => {
-            const query = { status: "pending" };
-            const cursor = blogsCollection.find(query);
-            const pendingBlogs = await cursor.toArray();
-            res.json(pendingBlogs);
-        });
-
-        // Get a single blog
-        app.get("/blogs/:id", async (req, res) => {
+        // Get a single product
+        app.get("/products/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const blog = await blogsCollection.findOne(query);
-            res.json(blog);
+            const product = await productsCollection.findOne(query);
+            res.json(product);
         });
+        // // Get order by id
+        // app.get("/orders/:id", async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const product = await ordersCollection.findOne(query);
+        //     res.json(product);
+        // });
 
         // Check if the user is admin or not
         app.get("/users/:email", async (req, res) => {
@@ -80,7 +71,37 @@ async function run() {
             res.json({ admin: isAdmin });
         });
 
+        // Get order by user
+        app.get("/orders/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const orders = ordersCollection.find(query);
+            const result = await orders.toArray();
+            res.json(result);
+        });
+
+        // Get orders
+        app.get("/orders", async (req, res) => {
+            const cursor = ordersCollection.find({});
+            const order = await cursor.toArray();
+            res.send(order);
+        });
+
+        // Get all reviews
+        app.get("/reviews", async (req, res) => {
+            const cursor = reviewCollection.find({});
+            const reviews = await cursor.toArray();
+            res.json(reviews);
+        });
+
         /* POST */
+
+        // Post a new order
+        app.post("/orders", async (req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.json(result);
+        });
 
         // add new user from registration form
         app.post("/users", async (req, res) => {
@@ -89,10 +110,17 @@ async function run() {
             res.json(result);
         });
 
-        // Add a new blog in the database
-        app.post("/blogs", async (req, res) => {
-            const blog = req.body;
-            const result = await blogsCollection.insertOne(blog);
+        // Add a new product in the database
+        app.post("/products", async (req, res) => {
+            const product = req.body;
+            const result = await productsCollection.insertOne(product);
+            res.json(result);
+        });
+
+        // Add a review
+        app.post("/reviews", async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
             res.json(result);
         });
 
@@ -122,54 +150,21 @@ async function run() {
             res.json(result);
         });
 
-        // Update a blog
-        app.put("/blogs/:id", async (req, res) => {
-            console.log("api hit");
-            const id = req.params.id;
-            const updatedBlog = req.body;
-            const query = { _id: ObjectId(id) };
-            const options = { upsert: true };
-            const updateDoc = {
-                $set: {
-                    status: updatedBlog.status,
-                    blogTitle: updatedBlog.blogTitle,
-                    blogImg: updatedBlog.blogImg,
-                    traveller: updatedBlog.traveller,
-                    cost: updatedBlog.cost,
-                    category: updatedBlog.category,
-                    location: updatedBlog.location,
-                    description: updatedBlog.description,
-                },
-            };
-            const result = await blogsCollection.updateOne(
-                query,
-                updateDoc,
-                options
-            );
-            res.json(result);
-        });
-
-        // Approve a pending blog
-        app.put("/blogs/:id", async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: ObjectId(id) };
-            const options = { upsert: true };
-            const updateDoc = { $set: { status: "approved" } };
-            const result = await blogsCollection.updateOne(
-                filter,
-                updateDoc,
-                options
-            );
-            res.json(result);
-        });
-
         /* DELETE */
 
-        // Delete a blog based on Id
-        app.delete("/blogs/:id", async (req, res) => {
+        // Delete a order based on Id
+        app.delete("/orders/:id", async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const result = await blogsCollection.deleteOne(query);
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        });
+
+        // Delete a product based on Id
+        app.delete("/products/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
             res.json(result);
         });
     } finally {
@@ -179,9 +174,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("Travellers Hut server is running");
+    res.send("Baby Land server is running");
 });
 
 app.listen(port, () => {
-    console.log("Travellers Hut server is running on:", port);
+    console.log("Baby Land server is running on:", port);
 });
